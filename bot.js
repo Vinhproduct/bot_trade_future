b6fd5c7435afc154103b86349be151224c41c2ea
Vinh-fullStack-developer <135152238+Vinhproduct@users.noreply.github.com>
2025-05-05 08:47:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,10 +14,10 @@ const exchange = new ccxt.binance({
 
 // Cấu hình bot
 const maxPositions = 10;
-const tradeAmount = 10.0000; // Each trade is exactly $10
+const tradeAmount = 10; // Each trade is exactly $10
 const leverage = 7;
-const profitTarget = 3;
-const lossLimit = 4;
+const profitTarget = 2;
+const lossLimit = 3;
 const rsiPeriod = 14;
 const smaPeriod = 50;
 const emaPeriod = 20;
