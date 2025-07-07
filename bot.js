3949e4cc184b29795f262c344acf6807567826f3
Vinh-fullStack-developer <135152238+Vinhproduct@users.noreply.github.com>
2025-07-07 18:54:21 +0700

    bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const exchange = new ccxt.binance({
 
 // Cấu hình bot
 const maxPositions = 5;
-const tradeAmount = 10; // Mỗi lệnh $10
+const tradeAmount = 12; // Mỗi lệnh $10
 const leverage = 5; // Đòn bẩy
 const profitTarget = 2; // Mục tiêu lợi nhuận $2
 const lossLimit = 3; // Giới hạn lỗ $3
